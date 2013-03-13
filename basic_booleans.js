671c2f56ef41460c2f4142b1b0023345bb782b08
chamy <chamy.bluemoon@gmail.com>
2013-03-13 08:59:26 -0400

    new file:   basic_booleans.js 	new file:   basic_strings.js

---
+++ basic_booleans.js
@@ -0,0 +1,19 @@
+console.log('--------------------');
+console.log(' type of boolean');
+console.log('--------------------');
+
+if ( undefined ) {
+    console.log('undefined is true');
+} else {
+    console.log('undefined is false');
+}
+
+if ( null ) {
+    console.log('null is true');
+} else {
+    console.log('null is false');
+}
+
+
+console.log('!NaN is '+ !NaN.toString());
+console.log('true is '+ "true".toString());
---
+++ basic_strings.js
@@ -0,0 +1,17 @@
+console.log('--------------------');
+console.log(' type of string');
+console.log('--------------------');
+
+var p = "π"; //U+03c0
+var e = "𝑒"; //U+1πD452
+
+if ( p.length == e.length ){
+    console.log(' p & e length is ' + p.length );
+} else{
+    console.log(' p length is ' + p.length);
+    console.log(' e length is ' + e.length);
+}
+
+var zero = "";
+
+    console.log(' zero length is ' + zero.length);
